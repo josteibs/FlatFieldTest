1a7593904ed277529403bfaaad18359f61cbbe75
Steffensen <josste@ihelse.net>
2022-07-04 15:56:12 +0200

    Started to add canon scanner. Some functions made to be more general, so less code is needed. Drop down menus are updated with scanner and reconstruction choice. Not all functionallity is possible to use for the canon yet, because some NPS files and dicoms are missing for certain doses and filters.

--- NPS_Dose_plot.py
+++ NPS_Dose_plot.py
@@ -13,20 +13,31 @@ import PySimpleGUI as sg
 import pydicom
 import glob
 
+#Available scanners and filters
+scanner = ['Siemens AS+', 'Siemens Flash', 'Canon Prime']
+filter_FBP_siemens = ['H10s', 'H20s', 'H30s', 'H37s', 'H40s', 'H50s', 'H60s', 'H70h']
+filter_IR_siemens = ['J30s', 'J37s', 'J40s', 'J45s', 'J49s', 'J70h', 'Q30s', 'Q33s']
+filter_canon = ['FC20', 'FC21', 'FC22', 'FC23', 'FC24', 'FC25', 'FC26']
+
+##############################################
+#Layout
+##############################################
 layout = [
-    #reconstructions available
+    #scanners
     [sg.Text("Pick scanner: ")], 
-    [sg.Combo(['Siemens AS+', 'Siemens Flash'], default_value = 'Siemens AS+', key='scanner')],
+    [sg.Combo(scanner, enable_events = True, key='scanner', size=(15,0))],
+    #reconstructions
     [sg.Text("Choose reconstruction: ")],
-    [sg.Combo(['FBP', 'IR1', 'IR2', 'IR4'], default_value = 'FBP', key='rec_type')],
+    [sg.Combo([], enable_events = True, key='rec_type', size = (15,0))],
     #All filters available
     [sg.Text("Choose filter: ")], 
-    [sg.Combo(['H10s', 'H20s', 'H30s', 'H37s', 'H40s', 'H50s', 'H60s', 'H70h'], default_value = 'H10s', key='filter_type'), 
-     sg.Combo(['J30s', 'J37s', 'J40s', 'J45s', 'J49s', 'J70h', 'Q30s', 'Q33s'], default_value = 'J30s', key='filter_type_IR')], 
-    [sg.Button('Plot NPS', key='-NPS_BUTTON-')],
-    [],
-    [sg.Button('Plot all FBP', key='-FBP_ALL-'), sg.Button('Plot all IR1', key='-IR1_ALL-'), sg.Button('Plot all IR2', key='-IR2_ALL-'), sg.Button('Plot all IR4', key='-IR4_ALL-')],
-    [sg.Text("Choose dose level: "), sg.Combo(['40 mGy', '60 mGy', '80 mGy', 'ALL'], default_value = 'ALL', key='dose_level'), sg.Button('Test image', key='-IMAGE-')]
+    [sg.Combo([], key='filter_type', size=(15,1))], 
+    #NPS plots
+    [sg.Button('Plot NPS', key='-NPS_simple-', disabled = True, size=(15,1))],
+    [sg.Button('Plot NPS all filters', key='-NPS_ALL-', disabled = True, size=(15,0))],
+    #Show image
+    [sg.Text("Choose dose level: "), sg.Combo(['40 mGy', '60 mGy', '80 mGy', 'ALL'], default_value = 'ALL', key='dose_level'), 
+     sg.Button('Test image', key='-IMAGE-')]
     
 ]
 
@@ -78,73 +89,34 @@ def plot_NPS_dose(scanner_name, filter_name, reconstruction, onePlot=True, index
     if onePlot==True:
         plt.show()
     
-def plot_NPS_dose_FBP_all():
+def plot_NPS_dose_all():
     #Plotting all NPS vs Dose for FBP
     onePlot=False
     fig = plt.figure()
-    fig.suptitle(value['scanner'] + ' with ' + 'FBP', fontsize=16)
+    fig.suptitle(values['scanner'] + ' with ' + values['rec_type'], fontsize=16)
     #
-    plot_NPS_dose(value['scanner'], 'H10s', 'FBP', onePlot, 1)
-    plot_NPS_dose(value['scanner'], 'H20s', 'FBP', onePlot, 2)
-    plot_NPS_dose(value['scanner'], 'H30s', 'FBP', onePlot, 3)
-    plot_NPS_dose(value['scanner'], 'H37s', 'FBP', onePlot, 4)
-    plot_NPS_dose(value['scanner'], 'H40s', 'FBP', onePlot, 5)
-    plot_NPS_dose(value['scanner'], 'H50s', 'FBP', onePlot, 6)
-    plot_NPS_dose(value['scanner'], 'H60s', 'FBP', onePlot, 7)
-    plot_NPS_dose(value['scanner'], 'H70h', 'FBP', onePlot, 8)
-    plt.show()
+    if values['scanner'] == 'Siemens AS+' or values['scanner'] == 'Siemens Flash':
+        if values['rec_type']=='FBP':
+            filter_list = filter_FBP_siemens
     
-def plot_NPS_dose_IR1_all():
-    #Plotting all NPS vs Dose for IR1
-    onePlot=False
-    fig = plt.figure()
-    fig.suptitle(value['scanner'] + ' with ' + 'IR1', fontsize=16)
+        else:
+            filter_list = filter_IR_siemens
+        
+    elif values['scanner'] == 'Canon Prime':
+        filter_list = filter_canon
     #
-    plot_NPS_dose(value['scanner'], 'J30s', 'IR1', onePlot, 1)
-    plot_NPS_dose(value['scanner'], 'J37s', 'IR1', onePlot, 2)
-    plot_NPS_dose(value['scanner'], 'J40s', 'IR1', onePlot, 3)
-    plot_NPS_dose(value['scanner'], 'J45s', 'IR1', onePlot, 4)
-    plot_NPS_dose(value['scanner'], 'J49s', 'IR1', onePlot, 5)
-    plot_NPS_dose(value['scanner'], 'J70h', 'IR1', onePlot, 6)
-    plot_NPS_dose(value['scanner'], 'Q30s', 'IR1', onePlot, 7)
-    plot_NPS_dose(value['scanner'], 'Q33s', 'IR1', onePlot, 8)
+    plot_NPS_dose(values['scanner'], filter_list[0], values['rec_type'], onePlot, 1)
+    plot_NPS_dose(values['scanner'], filter_list[1], values['rec_type'], onePlot, 2)
+    plot_NPS_dose(values['scanner'], filter_list[2], values['rec_type'], onePlot, 3)
+    plot_NPS_dose(values['scanner'], filter_list[3], values['rec_type'], onePlot, 4)
+    plot_NPS_dose(values['scanner'], filter_list[4], values['rec_type'], onePlot, 5)
+    plot_NPS_dose(values['scanner'], filter_list[5], values['rec_type'], onePlot, 6)
+    plot_NPS_dose(values['scanner'], filter_list[6], values['rec_type'], onePlot, 7)
+    try:
+        plot_NPS_dose(values['scanner'], filter_list[7], values['rec_type'], onePlot, 8)
+    except:
+        pass
     plt.show()
-    
-def plot_NPS_dose_IR2_all():
-    #Plotting all NPS vs Dose for IR2
-    onePlot=False
-    fig = plt.figure()
-    fig.suptitle(value['scanner'] + ' with ' + 'IR2', fontsize=16)
-    #
-    plot_NPS_dose(value['scanner'], 'J30s', 'IR2', onePlot, 1)
-    plot_NPS_dose(value['scanner'], 'J37s', 'IR2', onePlot, 2)
-    plot_NPS_dose(value['scanner'], 'J40s', 'IR2', onePlot, 3)
-    plot_NPS_dose(value['scanner'], 'J45s', 'IR2', onePlot, 4)
-    plot_NPS_dose(value['scanner'], 'J49s', 'IR2', onePlot, 5)
-    plot_NPS_dose(value['scanner'], 'J70h', 'IR2', onePlot, 6)
-    plot_NPS_dose(value['scanner'], 'Q30s', 'IR2', onePlot, 7)
-    plot_NPS_dose(value['scanner'], 'Q33s', 'IR2', onePlot, 8)
-    plt.show()
-    
-def plot_NPS_dose_IR4_all():
-    #Plotting all NPS vs Dose for IR2
-    onePlot=False
-    fig = plt.figure()
-    fig.suptitle(value['scanner'] + ' with ' + 'IR4', fontsize=16)
-    #
-    plot_NPS_dose(value['scanner'], 'J30s', 'IR4', onePlot, 1)
-    plot_NPS_dose(value['scanner'], 'J37s', 'IR4', onePlot, 2)
-    plot_NPS_dose(value['scanner'], 'J40s', 'IR4', onePlot, 3)
-    plot_NPS_dose(value['scanner'], 'J45s', 'IR4', onePlot, 4)
-    plot_NPS_dose(value['scanner'], 'J49s', 'IR4', onePlot, 5)
-    plot_NPS_dose(value['scanner'], 'J70h', 'IR4', onePlot, 6)
-    plot_NPS_dose(value['scanner'], 'Q30s', 'IR4', onePlot, 7)
-    plot_NPS_dose(value['scanner'], 'Q33s', 'IR4', onePlot, 8)
-    plt.show()
-
-##############################################
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-##############################################
 
 ##############################################
 #Image functions
@@ -164,12 +136,15 @@ def show_dicom(scanner_name, filter_name, reconstruction, dose_level):
     '''
     Displays the 6th image in a sequence for a certain dose, filter and reconstruction.
     '''
-    image_path = "../CT bilder av Catphan/"+ scanner_name +"/" + dose_dict[dose_level] + " " + reconstruction + "  3.0  " + filter_name + "/*" #220623 JBS Changed from *.dcm to * to include other image formats. 
+    if (scanner_name == 'Siemens AS+' or scanner_name == 'Siemens Flash'):
+        
+        image_path = "../CT bilder av Catphan/"+ scanner_name +"/" + dose_dict[dose_level] + " " + reconstruction + "  3.0  " + filter_name + "/*" #220623 JBS Changed from *.dcm to * to include other image formats. 
     
-    print(image_path)
+    elif scanner_name == 'Canon Prime':
+        image_path = "../CT bilder av Catphan/"+ scanner_name +"/" + dose_dict[dose_level] + "/" + reconstruction + "/" + filter_name + "/*"
+    
     sortDict, sortedKeys = sortImages(image_path) #Sort images
     
-    print(sortDict)
     final_path = sortDict[sortedKeys[5]]
     
     dataset = pydicom.dcmread(final_path)
@@ -181,63 +156,99 @@ def show_dicom(scanner_name, filter_name, reconstruction, dose_level):
 
 def show_dicom_all_dose():
     fig = plt.figure()
-    filter_name = ''
-    if value['rec_type']=='FBP':
-        filter_name = value['filter_type']
-    else:
-        filter_name = value['filter_type_IR']
         
-    fig.suptitle(f"{value['scanner']} with {value['rec_type']} and {filter_name}" , color='white', fontsize=16)
+    fig.suptitle(f"{values['scanner']} with {values['rec_type']} and {values['filter_type']}" , color='white', fontsize=16)
     plt.subplot(1,3,1)
-    show_dicom(value['scanner'], filter_name, value['rec_type'], '40 mGy')
+    show_dicom(values['scanner'], values['filter_type'], values['rec_type'], '40 mGy')
     plt.subplot(1,3,2)
-    show_dicom(value['scanner'], filter_name, value['rec_type'], '60 mGy')
+    show_dicom(values['scanner'], values['filter_type'], values['rec_type'], '60 mGy')
     plt.subplot(1,3,3)
-    show_dicom(value['scanner'], filter_name, value['rec_type'], '80 mGy')
+    show_dicom(values['scanner'], values['filter_type'], values['rec_type'], '80 mGy')
     
-##############################################
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-##############################################
-
-    
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################    
+###############################################################################
+# GUI action
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################    
 while True:
-    event,value= window.read()
+    event, values = window.read()
     
-    #if value['rec_type'] == 'IR1' or value['rec_type'] == 'IR1':
+    ##############################################################
+    #Update GUI
+    ##############################################################
+    #Scanner choice 
+    if event == 'scanner':
+        if values[event] == 'Siemens AS+':
+            rec_liste = ['FBP', 'IR1', 'IR2', 'IR4']
+            window['rec_type'].update(value = '', values = rec_liste)
+            window['filter_type'].update(value = '', values = [])
+            
+        elif values[event] == 'Siemens Flash':
+            rec_list = ['FBP', 'IR1', 'IR2']
+            window['rec_type'].update(value = '', values = rec_list)
+            window['filter_type'].update(value ='', values = [])
+            
+        elif values[event] == 'Canon Prime':
+            rec_liste = ['ORG', 'AIDR 3D STD', 'AIDR 3D eSTD']
+            window['rec_type'].update(value = '', values = rec_liste)
+            window['filter_type'].update(value ='', values = [])
+            
+        #Disables NPS button if scanner is switched. 
+        window['-NPS_simple-'].update(disabled = True)
+        window['-NPS_ALL-'].update(disabled = True)
         
+    #Reconstruction choice 
+    if event == 'rec_type':
+        if (values['scanner']=='Siemens AS+' or values['scanner']=='Siemens Flash'):
+            if values[event] == 'FBP':
+                filter_list = filter_FBP_siemens
+                window['filter_type'].update(value = filter_list[0], values = filter_list)
+            else:
+                filter_list = filter_IR_siemens
+                window['filter_type'].update(value = filter_list[0], values = filter_list)   
+                
+        elif values['scanner']=='Canon Prime':
+            filter_list = filter_canon
+            window['filter_type'].update(value = filter_list[0], values = filter_list)
+        
+        
+        #Enables button if rec and filter is choosen. 
+        window['-NPS_simple-'].update(disabled = False)
+        window['-NPS_ALL-'].update(disabled = False)
+    
+        
+    ##############################################################
+    #NPS PLOTTING
+    ##############################################################
+    
     #For spesific NPS-button press. 
-    if event == '-NPS_BUTTON-':
-        #Make sure file exist.
-        if value['rec_type']=='FBP':
-            plot_NPS_dose(value['scanner'], value['filter_type'], value['rec_type'])
-            
-        else:
-            plot_NPS_dose(value['scanner'], value['filter_type_IR'], value['rec_type'])
-            
-    #When one of the "plot all"-buttons are pressed.
-    if event == '-FBP_ALL-':
-        plot_NPS_dose_FBP_all()
-    
-    if event == '-IR1_ALL-':
-        plot_NPS_dose_IR1_all()
-    
-    if event == '-IR2_ALL-':
-        plot_NPS_dose_IR2_all()
-    
-    if event == '-IR4_ALL-':
-        plot_NPS_dose_IR4_all()
+    if event == '-NPS_simple-':
+        plot_NPS_dose(values['scanner'], values['filter_type'], values['rec_type'])
         
+            
+    #NPS plot for all filters.
+    if event == '-NPS_ALL-':
+        plot_NPS_dose_all()
+    
+        
+    ##############################################################
+    #NPS PLOTTING
+    ##############################################################
+    
     #IMAGES FOR CERTAIN DOSE
-    if event == '-IMAGE-' and not value['dose_level']=='ALL':
-        #Make sure file exist
+    if event == '-IMAGE-' and not values['dose_level']=='ALL':
         plt.figure()
-        if value['rec_type']=='FBP':
-            show_dicom(value['scanner'], value['filter_type'], value['rec_type'], value['dose_level'])
-        else:
-            show_dicom(value['scanner'], value['filter_type_IR'], value['rec_type'], value['dose_level'])
+        show_dicom(values['scanner'], values['filter_type'], values['rec_type'], values['dose_level'])
+        
     
     #IMAGES FOR ALL DOSES
-    if event == '-IMAGE-' and value['dose_level']=='ALL':
+    if event == '-IMAGE-' and values['dose_level']=='ALL':
         show_dicom_all_dose()
     
     #break
